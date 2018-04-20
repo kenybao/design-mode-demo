c24241267a710ce313fba135100ddf31c5c7ef83
kenybao <735980892@qq.com>
2018-04-20 15:09:12 +0800

    Initial commit

---
+++ src/main/java/bbs/designmodedemo/DesignModeDemoApplication.java
@@ -0,0 +1,12 @@
+package bbs.designmodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignModeDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/bbs/designmodedemo/DesignModeDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package bbs.designmodedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesignModeDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
